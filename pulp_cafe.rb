690b8c237b425c7be5d4f5bb2010cc0b0c489ef4
ekliot <ekliot@gmail.com>
2017-09-12 10:13:12 -0400

    pre-departure

--- pulp_cafe.rb
+++ pulp_cafe.rb
@@ -7,7 +7,7 @@ require 'chatterbot/dsl'
 # Enabling **debug_mode** prevents the bot from actually sending
 # tweets. Keep this active while you are developing your bot. Once you
 # are ready to send out tweets, you can remove this line.
-debug_mode
+# debug_mode
 
 # Chatterbot will keep track of the most recent tweets your bot has
 # handled so you don't need to worry about that yourself. While
@@ -57,11 +57,11 @@ def start
     snooze = true
   end
   # @data = JSON.parse( IO.read 'arrival.json' )
-  @data = JSON.parse( IO.read 'visitor.json' )
+  # @data = JSON.parse( IO.read 'visitor.json' )
   # @data = JSON.parse( IO.read 'departure.json' )
 
-  # if !snooze then tweet generate end
-  (1..10).each do tweet generate; puts "\n\n" end
+  if !snooze then tweet generate end
+  # (1..10).each do tweet generate; puts "\n\n" end
 end
 
 
